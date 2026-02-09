5a0810870c5a7347b5e06fb7e82caee6d3bbbbe9
Muhammad Farooq <fm6182218@gmail.com>
2026-02-09 13:54:30 +0500

    Done

--- script.js
+++ script.js
@@ -2,31 +2,34 @@ const apiKey = "bc949a445487eef221aa464bfb7c2c6c";
 
 const forecastDiv = document.getElementById("forecast");
 const btn = document.getElementById("getForecast");
+const cityInput = document.getElementById("city");
 
 btn.addEventListener("click", () => {
-  const city = document.getElementById("city").value;
+
+  const city = cityInput.value;
   if (!city) {
     alert("Please enter a city name");
     return;
   }
+
 
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      console.log(city);
+      // console.log(data);
+      // console.log(city);
 
       forecastDiv.innerHTML = "";
       data.list[0].dt_txt;
       const dailyData = data.list.filter((item) =>
         item.dt_txt.includes("12:00:00")
       );
-      console.log(dailyData);
+      // console.log(dailyData);
 
       dailyData.forEach((val) => {
-        console.log(val);
+        // console.log(val);
 
         const date = new Date(val.dt_txt).toDateString();
         const temp = Math.round(val.main.temp);
@@ -40,6 +43,7 @@ btn.addEventListener("click", () => {
                 <p class="text-gray-500">${desc}</p>
             </div>`;
         forecastDiv.innerHTML += card;
+        cityInput.value = ""
       });
     })
     .catch((err) => {
